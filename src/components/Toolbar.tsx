944fe5282dfaa3b3e63ff68bbc1db2187627d55f
Artyom Perevalov <lovetraindriver@gmail.com>
2022-06-30 19:23:27 +0700

    eraser and oval added

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import canvasState from "../store/canvasState";
 import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
+import Eraser from "../tools/Eraser";
+import Oval from "../tools/Oval";
+import Path from "../tools/Path";
 import Rectangle from "../tools/Rectangle";
 import './../styles/toolbar.sass'
 
@@ -10,9 +13,9 @@ const Toolbar = () => {
         <div className="toolbar">
             <button className="toolbar__btn" onClick={()=>{toolState.setTool(new Brush(canvasState.canvas))}}>Brush</button>
             <button className="toolbar__btn" onClick={()=>{toolState.setTool(new Rectangle(canvasState.canvas))}}>Rect</button>
-            <button className="toolbar__btn">Circle</button>
-            <button className="toolbar__btn">Eraser</button>
-            <button className="toolbar__btn">Line</button>
+            <button className="toolbar__btn" onClick={()=>{toolState.setTool(new Oval(canvasState.canvas))}}>Oval</button>
+            <button className="toolbar__btn" onClick={()=>{toolState.setTool(new Eraser(canvasState.canvas))}}>Eraser</button>
+            <button className="toolbar__btn" onClick={()=>{toolState.setTool(new Path(canvasState.canvas))}}>Line</button>
             <input type="color" />
             <button className="toolbar__btn" style={{'marginLeft': 'auto'}}>Undo</button>
             <button className="toolbar__btn">Redo</button>
